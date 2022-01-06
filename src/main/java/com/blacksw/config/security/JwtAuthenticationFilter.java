3ddf498a78396794de655fc2e0fab957f0cb0b1c
suwan <blacksw1102@gmail.com>
2022-01-07 07:18:05 +0900

    쓰레기 코드 제거

--- src/main/java/com/blacksw/config/security/JwtAuthenticationFilter.java
+++ src/main/java/com/blacksw/config/security/JwtAuthenticationFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
@@ -24,10 +23,6 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
 
-		if(jwtProvider == null) {
-			System.out.println("@@");
-		}
-		
 		String token = jwtProvider.resolveToken((HttpServletRequest) request);
 		if(token != null && jwtProvider.validationToken(token)) {
 			Authentication authentication = jwtProvider.getAuthentication(token);
